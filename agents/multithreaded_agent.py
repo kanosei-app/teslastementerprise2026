4e304e40b61fcd31164a403a855639abb4d05452
Timur Tillabaev <tillabaev.tim@gmail.com>
2026-04-20 17:29:27 -0700

    Updated legacy import helper
    Removed _compat and legacy importing for faster load times

--- agents/multithreaded_agent.py
+++ agents/multithreaded_agent.py
@@ -1,14 +1,12 @@
-from ._compat import import_legacy_ceo_agents_module
-
-_legacy = import_legacy_ceo_agents_module("multithreaded_agent")
-
-MultiTopicAgentCoordinator = _legacy.MultiTopicAgentCoordinator
-ParallelAgentRuntime = _legacy.ParallelAgentRuntime
-TopicListener = _legacy.TopicListener
-make_topic_envelope = _legacy.make_topic_envelope
-send_messages_parallel = _legacy.send_messages_parallel
-topic_channel_name = _legacy.topic_channel_name
-wait_for_mailboxes_drained = _legacy.wait_for_mailboxes_drained
+from multithreaded_agent import (
+    MultiTopicAgentCoordinator,
+    ParallelAgentRuntime,
+    TopicListener,
+    make_topic_envelope,
+    send_messages_parallel,
+    topic_channel_name,
+    wait_for_mailboxes_drained,
+)
 
 __all__ = [
     "MultiTopicAgentCoordinator",
--- agents/thread_safe_agent.py
+++ agents/thread_safe_agent.py
@@ -1,7 +1,9 @@
-from ._compat import import_legacy_ceo_agents_module
+from thread_safe_agent import (
+    SupportsBusRegistration,
+    ThreadSafeAgentMixin,
+)
 
-_legacy = import_legacy_ceo_agents_module("thread_safe_agent")
-SupportsBusRegistration = _legacy.SupportsBusRegistration
-ThreadSafeAgentMixin = _legacy.ThreadSafeAgentMixin
-
-__all__ = ["SupportsBusRegistration", "ThreadSafeAgentMixin"]
+__all__ = [
+    "SupportsBusRegistration", 
+    "ThreadSafeAgentMixin"
+]
